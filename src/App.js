a968f8fbfb05b397c70aa82450d71cddafae44cd
Pawel Kaim <kaim.paul@gmail.com>
2018-01-24 20:44:35 +0000

    Created Admin, Profile and NewsFeed componnet templates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import NewsFeed from './components/NewsFeed'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <NewsFeed></NewsFeed>
       </div>
-    );
+
+    )
   }
 }
 
---
+++ src/containers/NewsFeed.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class NewsFeed extends Component {
+    render() {
+        return (
+          <h1>NewsFeed</h1>
+        )
+      }
+}
+
+export default NewsFeed
