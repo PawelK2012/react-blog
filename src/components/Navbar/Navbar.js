f269d631f28fb4ff53edb77a358db7b241d6cf59
Pawel Kaim <kaim.paul@gmail.com>
2018-01-25 13:57:56 +0000

    Removed .babelrc and fixed stateles componenet syntax for Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,37 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
+export const Navbar = () => (
+    <nav>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contactxxx</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
 
-// class Navbar extends Comment {
-//     render() {
-//         return (
-//             <nav>
-//                 <ul>
-//                     <li><a href="#home">Home</a></li>
-//                     <li><a href="#news">News</a></li>
-//                     <li><a href="#contact">Contact</a></li>
-//                     <li><a href="#about">About</a></li>
-//                 </ul>
+    </nav>
+)
 
-//             </nav>
-//         )
-//     }
-// }
-
-// export default Navbar
-
-const Navbar = () => {
-    return (
-        <nav>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#news">News</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#about">About</a></li>
-            </ul>
-
-        </nav>
-    )
-
-}
-
+// do i need this?
 export default Navbar
